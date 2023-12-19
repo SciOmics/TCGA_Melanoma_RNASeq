1d879093a1956c3ee51206c495e4afe4c97baf9c
David <david@The-Big-Mac.lan>
2023-12-19 14:45:36 -0500

    work in progress

--- Run.R
+++ Run.R
@@ -11,3 +11,16 @@
 
 
 ###ADD CODE TO RUN ALL ANALYSES### 
+
+#Check for tcga_skcm_data.RDS; if present do not run script 01
+
+TCGA_data_path = "data/tcga_skcm_data.RDS"
+
+if (file.exists(TCGA_data_path)) {
+  print("TCGA data present. Proceeding without downloading.")
+} else {
+  print("TCGA data not found. Proceeding to download data.")
+  source("code/01_Download_TCGA_Data.qmd")
+}
+
+#There is a problem running rmd/qmd files with source. Need to fix. 
